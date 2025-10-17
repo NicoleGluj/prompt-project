55de8493f673962d8ad11025d850b15c044292f6
NicoleGluj <nicolegluj@gmail.com>
2025-10-17 18:05:25 -0300

    add: clase 1 - estructrua base y conexion back express

---
+++ backend/src/index.js
@@ -0,0 +1,14 @@
+import express from "express"
+import fs from "fs" // file system
+import cors from "cors"
+
+const app = express()
+app.use(cors())
+
+const products = JSON.parse(fs.readFileSync("./src/data/products.json", "utf-8"))
+
+app.get("/", (req, res) => {
+  res.json(products)
+})
+
+app.listen(3000)
